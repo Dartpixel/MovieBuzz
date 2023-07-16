3526ec6070ec7980881c245a5d6a78b48270c021
Kartik Sharma <skartiksharma2004@gmail.com>
2023-07-16 11:32:08 +0530

    added

---
+++ src/Sdata.jsx
@@ -0,0 +1,28 @@
+const Sdata = [
+    {
+        imgscr: "sample1.jpg",
+        title: "Netflix Original Series",
+        height: '500px',
+        width: '850px',
+        sname: "Peaky Blinders",
+        link: "https://www.netflix.com/in/title/80002479"
+    },
+    {
+        imgscr: "sample2.jpg",
+        title: "Netflix Original Series",
+        height: '500px',
+        width: '850px',
+        sname: "Stranger Things",
+        link: "https://www.netflix.com/in/title/80057281",
+    },
+    {
+        imgsrc: "sample3.jpg",
+        title: "Netflix Original Series",
+        height: '500px',
+        width: '850px',
+        sname: "Breaking Bad",
+        link: "https://www.netflix.com/in/title/70143836",
+    }
+]
+
+export default Sdata;
